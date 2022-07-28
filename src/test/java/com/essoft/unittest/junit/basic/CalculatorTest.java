d31cce62649a2c19c2eee27ad013c77d4ad3786b
ErtanSidar <essoft3@gmail.com>
2022-07-28 18:03:25 +0300

    @DisplayName

---
+++ src/test/java/com/essoft/unittest/junit/basic/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.essoft.unittest.junit.basic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Calculator Tests")
+public class CalculatorTest {
+    @Test
+    @DisplayName("Additional of two numbers")
+    void additionalOfTwoIntegers() {
+
+    }
+    @Test
+    @DisplayName("Substractional of two numbers")
+    void substractionalOfTwoIntegers() {
+
+    }
+}
